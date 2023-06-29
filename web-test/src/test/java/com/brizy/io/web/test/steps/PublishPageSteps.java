5c47c355e6241086c86b3d8431465ddff8162274
Eugeniu Savca <savcaeugen@gmail.com>
2023-06-29 12:02:42 +0300

    * Added taking a screenshot to validate the items on page;

--- web-test/src/test/java/com/brizy/io/web/test/steps/PublishPageSteps.java
+++ web-test/src/test/java/com/brizy/io/web/test/steps/PublishPageSteps.java
@@ -1,11 +1,16 @@
 package com.brizy.io.web.test.steps;
 
 import com.brizy.io.web.interactions.properties.WebLocatorsProperties;
+import com.brizy.io.web.test.enums.StorageKey;
 import com.brizy.io.web.test.storage.Storage;
+import com.microsoft.playwright.Page;
 import io.cucumber.java.en.Then;
+import io.qameta.allure.Allure;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.ByteArrayInputStream;
 
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class PublishPageSteps {
@@ -19,6 +24,8 @@ public class PublishPageSteps {
 
     @Then("validate item properties")
     public void validateItemProperties() {
-
+        Page publishPage = storage.getValue(StorageKey.PUBLISH_PAGE, Page.class);
+        byte[] screenshot = publishPage.screenshot();
+        Allure.addAttachment("Published page", new ByteArrayInputStream(screenshot));
     }
 }
