9841f15d9bf35a8c6b92249991f915d8213d55bf
Eugeniu Savca <savcaeugen@gmail.com>
2023-10-19 14:53:46 +0300

    * Fixed imports.

--- web-interactions/src/main/java/com/brizy/io/web/interactions/constants/TimingConstants.java
+++ web-interactions/src/main/java/com/brizy/io/web/interactions/constants/TimingConstants.java
@@ -1,16 +1,13 @@
 package com.brizy.io.web.interactions.constants;
 
-import lombok.experimental.UtilityClass;
-
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
-@UtilityClass
-public class TimingConstants {
+public final class TimingConstants {
 
-    public Duration MAX_WAITING_TIME_FOR_FRAME_TO_LOAD = Duration.of(30, ChronoUnit.SECONDS);
-    public Duration POLL_DELAY_FOR_NUMERIC_VALUE_READ = Duration.of(500, ChronoUnit.MILLIS);
-    public Duration MIN_TIME_OF_WAITING_FOR_NUMERIC_VALUE_TO_APPLY = Duration.of(1, ChronoUnit.MILLIS);
-    public Duration MAX_TIME_OF_WAITING_FOR_NUMERIC_VALUE_TO_APPLY = Duration.of(10, ChronoUnit.SECONDS);
+    public static final Duration MAX_WAITING_TIME_FOR_FRAME_TO_LOAD = Duration.of(30, ChronoUnit.SECONDS);
+    public static final Duration POLL_DELAY_FOR_NUMERIC_VALUE_READ = Duration.of(500, ChronoUnit.MILLIS);
+    public static final Duration MIN_TIME_OF_WAITING_FOR_NUMERIC_VALUE_TO_APPLY = Duration.of(1, ChronoUnit.MILLIS);
+    public static final Duration MAX_TIME_OF_WAITING_FOR_NUMERIC_VALUE_TO_APPLY = Duration.of(10, ChronoUnit.SECONDS);
 
 }
